7d993b1e3158b57fb1dcd91c9201f76a491f1de6
HRITHIK MALLICK <66324529+hrithikmallick@users.noreply.github.com>
2021-09-18 20:26:03 +0530

    final work

--- rockpaper.py
+++ rockpaper.py
@@ -11,12 +11,32 @@ while True:
     user_inp = input('Type Rock/Paper/Scissers or Q to quit: ').lower()
 
     if user_inp == 'q':
-        print('Usere win', user_win, 'time', 'Computer win',
+        print('User win', user_win, 'time', 'Computer win',
               bot_win, 'time ')
         quit()
 
     random_number = random.randint(0, 2)
-    if user_inp == game[random_number]:
+    if user_inp == 'rock' and game[random_number] == 'scissers':
         user_win += 1
+        print('You win')
+        print('user input  ', user_inp,
+              'and computer input', game[random_number])
+    elif user_inp == 'paper' and game[random_number] == 'rock':
+        user_win += 1
+        print('You win')
+        print('user input  ', user_inp,
+              'and computer input', game[random_number])
+    elif user_inp == 'scissers' and game[random_number] == 'paper':
+        user_win += 1
+        print('You win')
+        print('user input  ', user_inp,
+              'and computer input', game[random_number])
+    elif user_inp == game[random_number]:
+        print("it's a draw")
+        print('user input  ', user_inp,
+              'and computer input', game[random_number])
     else:
         bot_win += 1
+        print('computer win')
+        print('user input  ', user_inp,
+              'and computer input', game[random_number])
